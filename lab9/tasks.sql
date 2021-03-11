1ba85b5f47bcc9fee66aef46a37ab83cb8d90d36
Alexei <krisfragerfeed@gmail.com>
2021-03-11 09:52:50 +0300

    feat: edit lab7,8 and start lab9

---
+++ lab9/tasks.sql
@@ -0,0 +1,9 @@
+use MAV_UNIVERTMP;
+DECLARE @i int = 1,
+		@c char(1) = 'a',
+		@b varchar(4) = 'BSTU',
+		@d datetime = '2021-12-27',
+		@t time = '12:03:12',
+		@si smallint = 3,
+		@ti tinyint = 1,
+		@n numeric(12,5);
